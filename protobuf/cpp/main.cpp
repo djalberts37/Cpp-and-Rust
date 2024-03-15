bdb2292a00217ddfd355d6c62d1eab78fa384e2e
Dennis J. Alberts <dennisalberts@hotmail.com>
2024-03-15 21:38:17 +0100

    Client calls server from C++ and receives reply. Updated the cmake list file to generate Greeter grpc classes.

--- protobuf/cpp/main.cpp
+++ protobuf/cpp/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "proto/helloworld.pb.h"
+#include "proto/helloworld.grpc.pb.h"
 #include <grpc/grpc.h> 
 #include <grpc++/channel.h>
 #include <grpc++/client_context.h>
@@ -13,10 +14,14 @@ int main() {
     std::cout << "Hello World" << std::endl;
 
     auto channel = grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
-    
-
+    auto client = helloworld::Greeter::NewStub(channel);
 
     helloworld::HelloRequest request;
+    helloworld::HelloReply reply;
+    ClientContext context;
     request.set_name("True Legends from C++!");
-    
+
+    client->SayHello(&context, request, &reply);
+
+    cout << "RESPONSE={" << reply.message() << "}" << endl;
 }
